4b1c176a1ec6b98d7cf90d0bcbaee5952fae6cea
Rodrigo Shimokawa <rodrigo.galvao@wolterskluwer.com>
2020-01-27 08:53:53 -0300

    Primeira versão e commit do app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Aplicação rodando via frameword express!');
+});
+
+app.listen(3000, function () {
+  console.log('Servidor rodando na porta 3000!');
+});
